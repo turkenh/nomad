a0457f43352a550e63177f68507ebe99641703e6
Michael Schurter <schmichael@hashicorp.com>
2017-02-15 14:16:01 -0800

    wip closer to building

--- command/agent/consul/client.go
+++ command/agent/consul/client.go
@@ -1,4 +1,3 @@
-// +build ignore
 package consul
 
 import (
@@ -15,13 +14,14 @@ import (
 	"github.com/hashicorp/nomad/nomad/structs"
 )
 
-const mark = struct{}{}
+var mark = struct{}{}
 
 type Executor interface {
 	Exec(ctx context.Context, cmd string, args []string) error
 }
 
 type Client struct {
+	client        *api.Client
 	retryInterval time.Duration
 	syncInterval  time.Duration
 
@@ -29,12 +29,12 @@ type Client struct {
 	syncCh     chan struct{}
 
 	// services and checks to be registered
-	regServices map[string]*api.AgentServiceRegisration
-	regChecks   map[string]*api.AgentServiceCheck
+	regServices map[string]*api.AgentServiceRegistration
+	regChecks   map[string]*api.AgentCheckRegistration
 
 	// services and checks to be unregisterd
-	deregServices map[string]string
-	deregChecks   map[string]string
+	deregServices map[string]struct{}
+	deregChecks   map[string]struct{}
 
 	// regLock must be held while accessing reg and dereg maps
 	regLock sync.Mutex
@@ -74,23 +74,126 @@ DISCO:
 	}
 }
 
+func (c *Client) sync() error {
+	// Shallow copy and reset the pending operations fields
+	c.regLock.Lock()
+	regServices := make(map[string]*api.AgentServiceRegistration, len(c.regServices))
+	for k, v := range c.regServices {
+		regServices[k] = v
+	}
+	c.regServices = map[string]*api.AgentServiceRegistration{}
+
+	regChecks := make(map[string]*api.AgentCheckRegistration, len(c.regChecks))
+	for k, v := range c.regChecks {
+		regChecks[k] = v
+	}
+	c.regChecks = map[string]*api.AgentCheckRegistration{}
+
+	deregServices := make(map[string]struct{}, len(c.deregServices))
+	for k := range c.deregServices {
+		deregServices[k] = mark
+	}
+	c.deregServices = map[string]struct{}{}
+
+	deregChecks := make(map[string]struct{}, len(c.deregChecks))
+	for k := range c.deregChecks {
+		deregChecks[k] = mark
+	}
+	c.deregChecks = map[string]struct{}{}
+	c.regLock.Unlock()
+
+	var err error
+
+	// Register Services
+	for id, service := range regServices {
+		if err = c.client.Agent().ServiceRegister(service); err != nil {
+			goto ERROR
+		}
+		delete(regServices, id)
+	}
+
+	// Register Checks
+	for id, check := range regChecks {
+		if err = c.client.Agent().CheckRegister(check); err != nil {
+			goto ERROR
+		}
+		delete(regChecks, id)
+	}
+
+	// Deregister Services
+	for id := range deregServices {
+		if err = c.client.Agent().ServiceDeregister(id); err != nil {
+			goto ERROR
+		}
+		delete(deregServices, id)
+	}
+
+	// Deregister Checks
+	for id := range deregChecks {
+		if err = c.client.Agent().CheckDeregister(id); err != nil {
+			goto ERROR
+		}
+		delete(deregChecks, id)
+	}
+	return nil
+
+	//TODO Labels and gotos are nasty; move to a function?
+ERROR:
+	// An error occurred, repopulate the operation maps omitting any keys
+	// that have been updated while sync() ran.
+	c.regLock.Lock()
+	for id, service := range regServices {
+		if _, ok := c.regServices[id]; ok {
+			continue
+		}
+		if _, ok := c.deregServices[id]; ok {
+			continue
+		}
+		c.regServices[id] = service
+	}
+	for id, check := range regChecks {
+		if _, ok := c.regChecks[id]; ok {
+			continue
+		}
+		if _, ok := c.deregChecks[id]; ok {
+			continue
+		}
+		c.regChecks[id] = check
+	}
+	for id, _ := range deregServices {
+		if _, ok := c.regServices[id]; ok {
+			continue
+		}
+		c.deregServices[id] = mark
+	}
+	for id, _ := range deregChecks {
+		if _, ok := c.regChecks[id]; ok {
+			continue
+		}
+		c.deregChecks[id] = mark
+	}
+	c.regLock.Unlock()
+	return err
+}
+
 // RegisterTask with Consul. Adds all sevice entries and checks to Consul.
 //
 // Actual communication with Consul is done asynchrously (see Run).
-func (c *Client) RegisterTask(allocID string, task *structs.Task, exec Executor) {
+func (c *Client) RegisterTask(allocID string, task *structs.Task, exec Executor) error {
 	regs := make([]*api.AgentServiceRegistration, len(task.Services))
-	checks := make([]*api.AgentServiceCheck, 0, len(task.Services)*2) // just guess at size
+	checks := make([]*api.AgentCheckRegistration, 0, len(task.Services)*2) // just guess at size
 
 	for i, service := range task.Services {
-		id := makeServiceKey(allocID, task.Name, service.Name)
-		host, port := task.AddrFinder(service.PortLabel)
-		regs[i] = &api.AgentServiceRegistration{
+		id := makeServiceKey(allocID, task.Name, service)
+		host, port := task.FindHostAndPortFor(service.PortLabel)
+		serviceReg := &api.AgentServiceRegistration{
 			ID:      id,
 			Name:    service.Name,
 			Tags:    service.Tags,
-			Address: addr,
+			Address: host,
 			Port:    port,
 		}
+		regs[i] = serviceReg
 
 		for _, check := range service.Checks {
 			if check.Type == structs.ServiceCheckScript {
@@ -102,13 +205,18 @@ func (c *Client) RegisterTask(allocID string, task *structs.Task, exec Executor)
 			if check.PortLabel != "" {
 				host, port = task.FindHostAndPortFor(check.PortLabel)
 			}
-			checks = append(checks, createCheckReg(service.ID, check, host, port))
+			checkReg, err := createCheckReg(id, check, host, port)
+			if err != nil {
+				return fmt.Errorf("failed to add check %q: %v", check.Name, err)
+			}
+			checks = append(checks, checkReg)
 		}
 
 	}
 
 	// Now add them to the registration fields
 	c.enqueueRegs(regs, checks)
+	return nil
 }
 
 // DeregisterTask from Consul. Removes all service entries and checks.
@@ -119,7 +227,7 @@ func (c *Client) RemoveTask(allocID string, task *structs.Task) {
 	checks := make([]string, 0, len(task.Services)*2) // just guess at size
 
 	for i, service := range task.Services {
-		id := makeServiceKey(allocID, task.Name, service.Name)
+		id := makeServiceKey(allocID, task.Name, service)
 		deregs = append(deregs, id)
 
 		for _, check := range service.Checks {
@@ -128,7 +236,7 @@ func (c *Client) RemoveTask(allocID string, task *structs.Task) {
 				c.stopCheck(check)
 				continue
 			}
-			checks = append(checks, check)
+			checks = append(checks, createCheckID(check, id))
 		}
 	}
 
@@ -136,7 +244,7 @@ func (c *Client) RemoveTask(allocID string, task *structs.Task) {
 	c.enqueueDeregs(deregs, checks)
 }
 
-func (c *Client) enqueueRegs(regs []*api.AgentServiceRegistration, checks []*api.AgentServiceCheck) {
+func (c *Client) enqueueRegs(regs []*api.AgentServiceRegistration, checks []*api.AgentCheckRegistration) {
 	c.regLock.Lock()
 	defer c.regLock.Unlock()
 	for _, reg := range regs {
@@ -170,44 +278,6 @@ func (c *Client) enqueueDeregs(deregs []string, checks []string) {
 	}
 }
 
-func (c *Client) AddServices(domain ServiceDomain, services []*structs.Service, advertise string, exec Executor) error {
-	host, port, err := net.SplitHostPort
-	for _, service := range services {
-		id := makeServiceKey(domain, service)
-		serviceReg := consul.AgentServiceRegistration{
-			ID:   id,
-			Name: service.Name,
-			Tags: service.Tags,
-		}
-
-		//TODO addrFinder :( Pass in to method? Pass in addr/port as args?
-		host, port := c.addrFinder(service.PortLabel)
-		if host != "" {
-			srv.Address = host
-		}
-
-		if port != 0 {
-			srv.Port = port
-		}
-
-		// Register the checks for this service
-		for _, check := range service.Checks {
-			//TODO
-			if check.Type == structs.ServiceCheckScript {
-				c.startCheck(check, exec)
-			}
-
-		}
-	}
-
-	//TODO Actually perform registration
-	//TODO Lock and add to map
-	//XXX  This approach will *only* work if reconciliation is blocked
-	//since if we register synchronously before adding to syncer's map
-	//there's a race where reconciliation might run and try to remove the
-	//created entries
-}
-
 func (c *Client) startCheck(check *structs.ServiceCheck, exec Executor) {
 	panic("TODO - run sevice check script in a goroutine")
 }
@@ -230,19 +300,23 @@ func makeServiceKey(allocID, taskName string, service *structs.Service) string {
 	return strings.Join(parts, "-")
 }
 
+func createCheckID(check *structs.ServiceCheck, serviceID string) string {
+	return check.Hash(serviceID)
+}
+
 // createCheckReg creates a Check that can be registered with Consul.
 //
 // Only supports HTTP(S) and TCP checks. Script checks must be handled
 // externally.
-func createCheckReg(serviceID string, check *structs.ServiceCheck, host string, port int) (*consul.AgentCheckRegistration, error) {
-	chkReg := consul.AgentCheckRegistration{
-		ID:        check.Hash(serviceID),
+func createCheckReg(serviceID string, check *structs.ServiceCheck, host string, port int) (*api.AgentCheckRegistration, error) {
+	chkReg := api.AgentCheckRegistration{
+		ID:        createCheckID(check, serviceID),
 		Name:      check.Name,
 		ServiceID: serviceID,
-		Status:    check.InitialStatus,
-		Timeout:   check.Timeout.String(),
-		Interval:  check.Interval.String(),
 	}
+	chkReg.Status = check.InitialStatus
+	chkReg.Timeout = check.Timeout.String()
+	chkReg.Interval = check.Interval.String()
 
 	switch check.Type {
 	case structs.ServiceCheckHTTP:
